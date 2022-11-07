bf454823840b9f9f15c8889b5d1db6e0ceee64ef
NataliiaVlasenko <108278401+NataliiaVlasenko@users.noreply.github.com>
2022-11-07 11:34:27 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
 
+ function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
+
+const bodyRef = document.querySelector("body");
+
+const startBtn = document.querySelector("button[data-start]");
+const stopBtn = document.querySelector("button[data-stop]");
+let timerId = null;
+
+stopBtn.setAttribute('disabled',true);
+
+startBtn.addEventListener("click", () => {
+    timerId = setInterval(() => {
+        bodyRef.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    startBtn.setAttribute('disabled',true);
+    stopBtn.removeAttribute('disabled');
+  });
+
+ 
+stopBtn.addEventListener("click", () => {
+  clearInterval(timerId);
+  startBtn.removeAttribute('disabled');
+  stopBtn.setAttribute('disabled',true);
+ });
