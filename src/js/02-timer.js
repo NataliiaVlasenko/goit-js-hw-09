71d4c49ce77a0602277903ce528f86f5a2588abb
NataliiaVlasenko <108278401+NataliiaVlasenko@users.noreply.github.com>
2022-11-08 15:00:40 +0200

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -32,7 +32,7 @@ const options = {
 
         startBtn.removeAttribute('disabled');
         startBtn.addEventListener('click', timerOn);
-        
+        dateInput.style.borderColor = "black";
     }
   },
 };
